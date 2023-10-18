ab9ea8a3816fd36c34ff6441fd9e70ee64005a7e
ofmxtheuuz <mxtheuzchika@gmail.com>
2023-10-18 18:47:42 -0300

    fix: fix login bug

--- src/main/java/dev/mxtheuz/bank/domain/dto/CreateUserDto.java
+++ src/main/java/dev/mxtheuz/bank/domain/dto/CreateUserDto.java
@@ -1,6 +1,5 @@
 package dev.mxtheuz.bank.domain.dto;
 
-import lombok.ToString;
 
 public record CreateUserDto(String name, String email, String password) {
 }
--- src/main/java/dev/mxtheuz/bank/domain/dto/LoginUserDto.java
+++ src/main/java/dev/mxtheuz/bank/domain/dto/LoginUserDto.java
@@ -1,6 +1,5 @@
 package dev.mxtheuz.bank.domain.dto;
 
-import lombok.ToString;
 
 public record LoginUserDto(String email, String password) {
 }
